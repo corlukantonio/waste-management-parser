537bb8f082ac73184981722cf52791a5a2d17a92
Antonio Ćorluka <corlukantonio@gmail.com>
2022-07-07 19:12:24 +0200

    Add project files.

---
+++ waste-management-parser/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace waste_management_parser.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
