48ffa8b5d3bcac10c84d20193e26d047b2eaab83
Antonio Ćorluka <corlukantonio@gmail.com>
2022-07-08 14:08:35 +0200

    Database init
    - Database init.

---
+++ waste-management-parser/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace waste_management_parser.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Display(Name = "Full name")]
+        public string FullName { get; set; }
+    }
+}
