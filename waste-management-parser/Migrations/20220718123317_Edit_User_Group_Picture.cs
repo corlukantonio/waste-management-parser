c29b6f983ee0e6305ee76034127f9fa4931e6e95
Antonio Ćorluka <acorluka@mobilisis.hr>
2022-07-18 15:05:48 +0200

    Pictures for WmGroup and User, auxiliary table
    - Edited models for WmGroup and ApplicationUser.
    - Auxiliary table for storing full waste bins.

---
+++ waste-management-parser/Migrations/20220718123317_Edit_User_Group_Picture.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace waste_management_parser.Migrations
+{
+    public partial class Edit_User_Group_Picture : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PictureData",
+                table: "WmGroups",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureName",
+                table: "WmGroups",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPasswordResetRequested",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PictureData",
+                table: "AspNetUsers",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureName",
+                table: "AspNetUsers",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureData",
+                table: "WmGroups");
+
+            migrationBuilder.DropColumn(
+                name: "PictureName",
+                table: "WmGroups");
+
+            migrationBuilder.DropColumn(
+                name: "IsPasswordResetRequested",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PictureData",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "PictureName",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ waste-management-parser/Migrations/20220718125952_Edit_WmObject_WasteBinForEmptying.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace waste_management_parser.Migrations
+{
+    public partial class Edit_WmObject_WasteBinForEmptying : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WmObjects_WasteBinForEmptying",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WmObjectId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WmObjects_WasteBinForEmptying", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WmObjects_WasteBinForEmptying_WmObjects_WmObjectId",
+                        column: x => x.WmObjectId,
+                        principalTable: "WmObjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WmObjects_WasteBinForEmptying_WmObjectId",
+                table: "WmObjects_WasteBinForEmptying",
+                column: "WmObjectId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WmObjects_WasteBinForEmptying");
+        }
+    }
+}
