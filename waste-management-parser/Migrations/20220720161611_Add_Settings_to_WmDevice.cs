b15b0457805aea68081bbb030db04c50db972c23
Antonio Ćorluka <acorluka@mobilisis.hr>
2022-07-20 18:33:17 +0200

    Settings added to WmObject
    - Settings added to WmObject.

---
+++ waste-management-parser/Migrations/20220720161611_Add_Settings_to_WmDevice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace waste_management_parser.Migrations
+{
+    public partial class Add_Settings_to_WmDevice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Settings",
+                table: "WmObjects",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Settings",
+                table: "WmObjects");
+        }
+    }
+}
