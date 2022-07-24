662fd28b66f8819ae552b758255fd128113d8566
Antonio Ćorluka <corlukantonio@gmail.com>
2022-07-24 22:08:33 +0200

    Implemented Login and Registration #1
    - Implemented Login and Registration.

---
+++ waste-management-parser/Controllers/GroupsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace waste_management_parser.Controllers
+{
+    [Authorize]
+    public class GroupsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ waste-management-parser/Controllers/ObjectsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace waste_management_parser.Controllers
+{
+    [Authorize]
+    public class ObjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
