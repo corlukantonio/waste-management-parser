b8abf834039b936b2f28d814bd72363187444a36
Antonio Ćorluka <corlukantonio@gmail.com>
2022-07-26 22:02:29 +0200

    Created a plain home page #1
    - Nothing on it except title.

---
+++ waste-management-parser/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace waste_management_parser.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
