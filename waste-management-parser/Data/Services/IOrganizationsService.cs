f806cd5ba96c722cd6eaca77c7b6d76a6aa40a62
Antonio Ćorluka <corlukantonio@gmail.com>
2022-07-27 02:13:29 +0200

    Get all organizations by user ID and user role #1
    - Get all organizations by user ID and user role.

---
+++ waste-management-parser/Data/Services/IOrganizationsService.cs
@@ -0,0 +1,10 @@
+﻿using waste_management_parser.Data.Base;
+using waste_management_parser.Models;
+
+namespace waste_management_parser.Data.Services
+{
+    public interface IOrganizationsService : IEntityBaseRepository<WmOrganization>
+    {
+        Task<List<WmOrganization>> GetOrganizationsByUserIdAndRoleAsync(string userId, string userRole);
+    }
+}
